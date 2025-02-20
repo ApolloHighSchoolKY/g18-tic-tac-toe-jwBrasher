2f9c17d48a926d759ede1e018327f7527b553a93
jwBrasher <john.brasher@stu.daviess.kyschools.us>
2025-02-20 05:52:02 +0000

    done

--- Lab24d.java
+++ Lab24d.java
@@ -14,8 +14,27 @@ public class Lab24d
 	{
 		Scanner file = new Scanner (new File("lab24d.dat"));
 
+		int x= file.nextInt();
+		
+		for(int i=0; i<=x; i++)
+		{
+			TicTacToe game = new TicTacToe(file.nextLine());
+		}
 
+		file.close();
+		
 
+	
+
+	//read how many times you should read
+	//next line
+	//file.nextLine()
+	//read number and then have the scanner read thar mamy times after a next line
+	//place into a 3x3 2d array with row makor order loop
+	//increase value of variable your tracking position to pull
+	//Make a TikTacToe object for each game
+	//Print the game()
+	//Print the winner
 
 
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -9,35 +9,74 @@ import static java.lang.System.*;
 
 public class TicTacToe
 {
-	private char[][] mat;
+	private char[][] board;
 
 	public TicTacToe()
 	{
-
-
+		char[][] board = new char[3][3];
 	}
 
 	public TicTacToe(String game)
 	{
+		int pos = 0;
+		//here is were the row major order loop happens
+		for(int col =0; col< board.length; col++)
+		{
+			for(int row = 0; row < board[col].length; row++)
+			{
+				board[col][row] = game.charAt(pos);
+				pos++;
+			}
+		}
 
+		
+			
 
 
 
 	}
 
 	public String getWinner()
-	{
+	{	//if the a=b and b=c then c=a
+		//only care if they are the same letter
+		//1st 2nd 3rd row. 1st 2nd 3rd column, 1st 2nd diagonal, e;se tie
+		
+		boolean win = false;
 
+		//ROWS
 
+		if(board[0][0]==board[0][1] && board[0][1]==board[0][2])
+			win=true;
 
+		if(board[1][0]==board[1][1] && board[1][1]==board[1][2])
+			win=true;
 
+		if(board[2][0]==board[2][1] && board[2][1]==board[2][2])
+			win=true;
 
+		//COLUMNS
 
+		if(board[0][0]==board[1][0] && board[1][0]==board[2][0])
+			win=true;
 
+		if(board[0][1]==board[1][1] && board[1][1]==board[2][1])
+			win=true;
 
+		if(board[0][2]==board[1][2] && board[1][2]==board[2][2])
+			win=true;
 
+		//DIOGONAL
+		
+		if(board[0][0]==board[1][1] && board[1][1]==board[2][2])
+			win=true;
 
-
+		if(board[0][2]==board[1][1] &&board[1][1]==board[2][0])
+			win=true;
+		
+		//TIE
+		
+		else
+		win = false;
 
 		return "";
 	}
@@ -45,12 +84,20 @@ public class TicTacToe
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
-
+	//also use row major order here
+	//add on character until row is complpete add a backslash n 
+	//then do it again
+	int pos = 0;
+	//here is were the row major order loop happens
+	for(int col =0; col< board.length; col++)
+	{
+		for(int row = 0; row < board[col].length; row++)
+		{
+			board[col][row] = game.charAt(pos);
+			output = board[col][row];
+			pos++;
+		}
+	}
 		return output+"\n\n";
 	}
 }
